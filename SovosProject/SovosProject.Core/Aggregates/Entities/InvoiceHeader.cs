90336dfeb386a11011cb85fdea1b45f00715a436
Yudum <yudum.savran@negzel.net>
2025-03-18 22:05:01 +0300

    Add InvoiceHeader and InvoiceLine entities

---
+++ SovosProject/SovosProject.Core/Aggregates/Entities/InvoiceHeader.cs
@@ -0,0 +1,21 @@
+﻿namespace SovosProject.Core.Aggregates.Entities
+{
+    public class InvoiceHeader
+    {
+        public string InvoiceId { get; private set; }
+        public string SenderTitle { get; private set; }
+        public string ReceiverTitle { get; private set; }
+        public DateTime Date { get;private set; }
+        public string Email { get;private set; }
+        public List<InvoiceLine> InvoiceLines { get; private set; } = new();
+
+        public InvoiceHeader(string invoiceId, string senderTitle, string receiverTitle, DateTime date, string email)
+        {
+            InvoiceId = invoiceId;
+            SenderTitle = senderTitle;
+            ReceiverTitle = receiverTitle;
+            Date = date;
+            Email = email;
+        }
+    }
+}
---
+++ SovosProject/SovosProject.Core/Aggregates/Entities/InvoiceLine.cs
@@ -0,0 +1,19 @@
+﻿namespace SovosProject.Core.Aggregates.Entities
+{
+    public class InvoiceLine
+    {
+        public int Id { get;private set; }
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+        public string UnitCode { get; private set; }
+        public decimal UnitPrice { get; private set; }
+        public InvoiceLine(int id, string name, int quantity, string unitCode, decimal unitPrice)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            UnitCode = unitCode;
+            UnitPrice = unitPrice;
+        }
+    }
+}
