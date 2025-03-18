1c599f4a0996056e97d0916b76e99311afeaaee8
Yudum <yudum.savran@negzel.net>
2025-03-18 22:55:05 +0300

    db connection with sqlserver and configure ApplicationDbContext

--- SovosProject/WebAPI/Program.cs
+++ SovosProject/WebAPI/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SovosProject.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SovosProjectDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  // Veritaban? ba?lant?s?
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
