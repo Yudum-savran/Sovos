a69370cab7811a0025ec300f2da1d44fcb8d8c09
Yudum <yudum.savran@negzel.net>
2025-03-19 17:55:28 +0300

    Added IMapper to InvoiceService, and methods were edited.

--- SovosProject/SovosProject.Application/Services/InvoiceService.cs
+++ SovosProject/SovosProject.Application/Services/InvoiceService.cs
@@ -1,4 +1,6 @@
-﻿using SovosProject.Application.Interfaces;
+﻿using AutoMapper;
+using SovosProject.Application.Interfaces;
+using SovosProject.Application.Models;
 using SovosProject.Core.Aggregates.Entities;
 using SovosProject.Infrastructure.Repository;
 
@@ -7,30 +9,20 @@ namespace SovosProject.Application.Services
     public class InvoiceService : IInvoiceService
     {
         private readonly IInvoiceRepository _invoiceRepository;
+        private readonly IMapper _mapper;
 
-        public InvoiceService(IInvoiceRepository invoiceRepository)
+        public InvoiceService(IInvoiceRepository invoiceRepository, IMapper mapper)
         {
             _invoiceRepository = invoiceRepository;
+            _mapper=mapper;
         }
-        public async Task AddInvoiceAsync(InvoiceHeader invoice)
+        public async Task AddInvoiceAsync(InvoiceHeaderDto value)
         {
             try
             {
-               if (invoice == null) throw new ArgumentNullException(nameof(invoice), "parameters invoice not found");
-                await _invoiceRepository.AddAsync(invoice);
-            }
-            catch (Exception ex)
-            {
-
-               throw new Exception("An error occurred while adding the invoice.", ex);
-            }
-        }
-
-        public async Task<List<InvoiceHeader>> GetAllInvoicesAsync()
-        {
-            try
-            {
-                return await _invoiceRepository.GetAllAsync();
+                if (value == null) throw new ArgumentNullException(nameof(value), "parameters invoice not found");
+                var invoiceHeader = _mapper.Map<InvoiceHeader>(value);
+                await _invoiceRepository.AddAsync(invoiceHeader);
             }
             catch (Exception ex)
             {
@@ -39,13 +31,37 @@ namespace SovosProject.Application.Services
             }
         }
 
-        public async Task<InvoiceHeader?> GetInvoiceByIdAsync(string invoiceId)
+        public async Task<List<InvoiceHeaderDto>> GetAllInvoicesAsync()
         {
             try
             {
-                if (invoiceId == null)
+                var invoices = await _invoiceRepository.GetAllAsync();
+                if (invoices == null || !invoices.Any())
+                    return new List<InvoiceHeaderDto>();
+
+                var invoiceDtos = _mapper.Map<List<InvoiceHeaderDto>>(invoices);
+                return invoiceDtos;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("An error occurred while adding the invoice.", ex);
+            }
+        }
+
+        public async Task<InvoiceHeaderDto?> GetInvoiceByIdAsync(string invoiceId)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(invoiceId))
                     throw new KeyNotFoundException("Parameters id is null object");
-                return await _invoiceRepository.GetByIdAsync(invoiceId);
+
+                var invoice = await _invoiceRepository.GetByIdAsync(invoiceId);
+                if (invoice == null) return null;
+
+                var invoiceDto = _mapper.Map<InvoiceHeaderDto>(invoice);
+                return invoiceDto;
             }
             catch (Exception ex)
             {
