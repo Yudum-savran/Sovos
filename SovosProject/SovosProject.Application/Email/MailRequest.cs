38bfee5e605cdaa7ca821d36b0e87b424a490ecf
Yudum <yudum.savran@negzel.net>
2025-03-19 23:05:41 +0300

    Added MailRequest class

---
+++ SovosProject/SovosProject.Application/Email/MailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SovosProject.Application.Email
+{
+    public class MailRequest
+    {
+        public string To { get; set; }
+        public string Subject { get; set; } = "Fatura Bilgilendirme Maili";
+        public string Body { get; set; }
+    }
+}
