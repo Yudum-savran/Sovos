ec4c8065da4c2cb0b959042fe870f431a744c530
Yudum <yudum.savran@negzel.net>
2025-03-19 17:52:04 +0300

    Added AutoMapper

---
+++ SovosProject/SovosProject.Application/AutoMappers/AutoMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using SovosProject.Application.Models;
+using SovosProject.Core.Aggregates.Entities;
+
+namespace SovosProject.Application.AutoMappers
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<InvoiceHeader, InvoiceHeaderDto>()
+          .ForMember(dest => dest.InvoiceLines, opt => opt.MapFrom(src => src.InvoiceLines));
+
+            CreateMap<InvoiceLine, InvoiceLineDto>();
+
+            CreateMap<InvoiceHeaderDto, InvoiceHeader>()
+       .ForMember(dest => dest.InvoiceLines, opt => opt.MapFrom(src => src.InvoiceLines))
+       .ForMember(dest => dest.Id, opt => opt.Ignore()); // ID'yi DB belirleyecek
+
+            CreateMap<InvoiceLineDto, InvoiceLine>()
+          .ForMember(dest => dest.Id, opt => opt.Ignore());// ID'yi DB belirleyecek
+        }
+
+    }
+}
