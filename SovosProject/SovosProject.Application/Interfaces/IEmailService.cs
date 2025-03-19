0435d60dfd750f903c7913e000246252117cc29f
Yudum <yudum.savran@negzel.net>
2025-03-19 23:07:44 +0300

    Added IEmailService

---
+++ SovosProject/SovosProject.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using SovosProject.Application.Email;
+
+namespace SovosProject.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
