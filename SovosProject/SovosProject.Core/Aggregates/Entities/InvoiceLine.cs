71013467e2b972fb3e460d761e3677430812a96f
Yudum <yudum.savran@negzel.net>
2025-03-19 17:56:30 +0300

    Edit entities

--- SovosProject/SovosProject.Core/Aggregates/Entities/InvoiceLine.cs
+++ SovosProject/SovosProject.Core/Aggregates/Entities/InvoiceLine.cs
@@ -1,4 +1,6 @@
-﻿namespace SovosProject.Core.Aggregates.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace SovosProject.Core.Aggregates.Entities
 {
     public class InvoiceLine
     {
@@ -7,7 +9,7 @@
         public int Quantity { get; private set; }
         public string UnitCode { get; private set; }
         public decimal UnitPrice { get; private set; }
-        public string InvoiceHeaderId { get; set; }
+        public int InvoiceHeaderId { get; set; }
         public InvoiceHeader InvoiceHeader { get; set; }
         public InvoiceLine(int id, string name, int quantity, string unitCode, decimal unitPrice)
         {
