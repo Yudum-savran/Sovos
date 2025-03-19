90853d1dcaed5f16a7f8b010521231dbaf80d2c4
Yudum <yudum.savran@negzel.net>
2025-03-19 17:58:14 +0300

    AddInvoice method edit in InvoiceController

--- SovosProject/WebAPI/Controllers/InvoiceController.cs
+++ SovosProject/WebAPI/Controllers/InvoiceController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SovosProject.Application.Services;
-using SovosProject.Core.Aggregates.Entities;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using SovosProject.Application.Interfaces;
+using SovosProject.Application.Models;
+using System;
 
 namespace WebAPI.Controllers
 {
@@ -8,17 +11,22 @@ namespace WebAPI.Controllers
     [Route("api/invoices")]
     public class InvoiceController : ControllerBase
     {
-        private readonly InvoiceService _invoiceService;
+        private readonly IInvoiceService _invoiceService;
+        private readonly IValidator<InvoiceHeaderDto> _invoiceValidator;
 
-        public InvoiceController(InvoiceService invoiceService)
+        public InvoiceController(IInvoiceService invoiceService, IValidator<InvoiceHeaderDto> invoiceValidator)
         {
             _invoiceService=invoiceService;
+            _invoiceValidator=invoiceValidator;
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddInvoice(InvoiceHeader invoice)
+        public async Task<IActionResult> AddInvoice(InvoiceHeaderDto value)
         {
-            await _invoiceService.AddInvoiceAsync(invoice);
+            ValidationResult validationResult = await _invoiceValidator.ValidateAsync(value);
+            if(!validationResult.IsValid) 
+                throw new ValidationException(validationResult.Errors);
+            await _invoiceService.AddInvoiceAsync(value);
             return Ok("Invoice added successfully");
         }
 
