2772449f54b5bc56ee7edf2ae41db3c5d0c77726
Yudum <yudum.savran@negzel.net>
2025-03-20 15:11:40 +0300

    Added EmailLogRepository

---
+++ SovosProject/SovosProject.Infrastructure/Repository/EmailLogRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SovosProject.Core.Entities;
+using SovosProject.Core.Repository;
+using SovosProject.Infrastructure.Data;
+
+namespace SovosProject.Infrastructure.Repository
+{
+    public class EmailLogRepository : IEmailLogRepository
+    {
+        private readonly SovosProjectDbContext _context;
+
+        public EmailLogRepository(SovosProjectDbContext context)
+        {
+            _context=context;
+        }
+        public async Task AddEmailLogAsync(MailLog mailLog)
+        {
+            await _context.MailLogs.AddAsync(mailLog);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
