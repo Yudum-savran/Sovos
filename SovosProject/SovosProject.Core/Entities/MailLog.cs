c38dd053d45634a59e47519e7926a3dbe95d171e
Yudum <yudum.savran@negzel.net>
2025-03-20 13:09:52 +0300

    Add MailLog entity

---
+++ SovosProject/SovosProject.Core/Entities/MailLog.cs
@@ -0,0 +1,14 @@
+﻿namespace SovosProject.Core.Entities
+{
+    public class MailLog
+    {
+        public int Id { get; set; }
+        public string FromEmail { get; set; }
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime SentDate { get; set; }
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
