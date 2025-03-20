e6abbf514b17ed68e0cb4b3602991da2c0e2544f
Yudum <yudum.savran@negzel.net>
2025-03-20 22:55:22 +0300

    edit IInvoiceService

--- SovosProject/SovosProject.Application/Interfaces/IInvoiceService.cs
+++ SovosProject/SovosProject.Application/Interfaces/IInvoiceService.cs
@@ -1,12 +1,13 @@
-﻿using SovosProject.Application.Models;
+﻿using SovosProject.Application.Common;
+using SovosProject.Application.Models;
 using SovosProject.Core.Entities;
 
 namespace SovosProject.Application.Interfaces
 {
     public interface IInvoiceService
     {
-        Task AddInvoiceAsync(InvoiceHeaderDto invoice);
-        Task<List<InvoiceHeaderDto>> GetAllInvoicesAsync();
-        Task<InvoiceHeaderDto?> GetInvoiceByIdAsync(string invoiceId);
+        Task<GenericResult<bool>> AddInvoiceAsync(InvoiceHeaderDto invoice);
+        Task<GenericResult<List<InvoiceHeaderDto>>> GetAllInvoicesAsync();
+        Task<GenericResult<InvoiceHeaderDto>> GetInvoiceByIdAsync(string invoiceId);
     }
 }
