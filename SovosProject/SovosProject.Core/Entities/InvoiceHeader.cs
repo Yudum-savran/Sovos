029ea543718e4a819993c3d997d3dca645166377
Yudum <yudum.savran@negzel.net>
2025-03-20 12:20:03 +0300

    Edited entities

---
+++ SovosProject/SovosProject.Core/Entities/InvoiceHeader.cs
@@ -0,0 +1,22 @@
+﻿namespace SovosProject.Core.Entities
+{
+    public class InvoiceHeader
+    {
+        public int Id { get; set; }
+        public string InvoiceId { get; set; }
+        public string SenderTitle { get; set; }
+        public string ReceiverTitle { get;  set; }
+        public DateTime Date { get; set; }
+        public string Email { get; set; }
+        public List<InvoiceLine> InvoiceLines { get; set; } = new();
+
+        public InvoiceHeader(string invoiceId, string senderTitle, string receiverTitle, DateTime date, string email)
+        {
+            InvoiceId = invoiceId;
+            SenderTitle = senderTitle;
+            ReceiverTitle = receiverTitle;
+            Date = date;
+            Email = email;
+        }
+    }
+}
---
+++ SovosProject/SovosProject.Core/Entities/InvoiceLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SovosProject.Core.Entities
+{
+    public class InvoiceLine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get;  set; }
+        public string UnitCode { get;  set; }
+        public decimal UnitPrice { get;  set; }
+        public int InvoiceHeaderId { get; set; }
+        public InvoiceHeader InvoiceHeader { get; set; }
+        public InvoiceLine(int id, string name, int quantity, string unitCode, decimal unitPrice)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            UnitCode = unitCode;
+            UnitPrice = unitPrice;
+        }
+    }
+}
