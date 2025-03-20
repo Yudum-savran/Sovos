e9fddc2f80bd5b60ec813290049315f78cf4ed68
Yudum <yudum.savran@negzel.net>
2025-03-20 22:55:08 +0300

    Add Error and GenericResult class

---
+++ SovosProject/SovosProject.Application/Common/GenericResult.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace SovosProject.Application.Common
+{
+    public class GenericResult<T>
+    {
+        public bool IsSuccess { get; }
+        public T Value { get; }
+        public Error Error { get; }
+
+        private GenericResult(bool isSuccess, T value, Error error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+        public static GenericResult<T> Success(T value) => new GenericResult<T>(true, value, null);
+        public static GenericResult<T> Failure(Error error) => new GenericResult<T>(false, default(T), error);
+    }
+}
