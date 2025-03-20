0cd871b15ac7fc9c5851490491873e44e8047986
Yudum <yudum.savran@negzel.net>
2025-03-20 18:53:36 +0300

    migration folder

--- SovosProject/SovosProject.Infrastructure/Migrations/20250320154840_AddProcessed.Designer.cs
+++ SovosProject/SovosProject.Infrastructure/Migrations/20250320154840_AddProcessed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SovosProject.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using SovosProject.Infrastructure.Data;
 namespace SovosProject.Infrastructure.Migrations
 {
     [DbContext(typeof(SovosProjectDbContext))]
-    partial class SovosProjectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250320154840_AddProcessed")]
+    partial class AddProcessed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SovosProject/SovosProject.Infrastructure/Migrations/20250320154840_AddProcessed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SovosProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Processed",
+                table: "InvoiceHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "Unprocessed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Processed",
+                table: "InvoiceHeaders");
+        }
+    }
+}
